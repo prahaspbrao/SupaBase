a7b4cb1ac59ab93ab986f7015952025edc104d8f
Prahas P B Rao <prahaspbrao2005@gmail.com>
2026-02-15 21:43:56 +0530

    CSS updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 // pages
-import Home from "./pages/Home"
-import Create from "./pages/Create"
-import Update from "./pages/Update"
-
+import Home from "./pages/Home";
+import Create from "./pages/Create";
+import Update from "./pages/Update";
 
 function App() {
   return (
     <BrowserRouter>
-      <nav>
-        <h1>Supa Smoothies</h1>
-        <Link to="/">Home</Link>
-        <Link to="/create">Create New Smoothie</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<Create />} />
-        <Route path="/:id" element={<Update />} />
-      </Routes>
+      <div className="app">
+        <nav className="navbar">
+          <h1 className="logo">🥤 Supa Smoothies</h1>
+
+          <div className="nav-links">
+            <Link to="/">Home</Link>
+            <Link to="/create" className="btn">
+              + New Smoothie
+            </Link>
+          </div>
+        </nav>
+
+        <main className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/:id" element={<Update />} />
+          </Routes>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
